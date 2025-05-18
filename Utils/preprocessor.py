2508fbad1d057c45516a0dbc6fb1f74782753dd5
donatooooooo <donatoboccuzzi@gmail.com>
2025-05-18 12:59:10 +0200

    cancer prediction task

---
+++ Utils/preprocessor.py
@@ -0,0 +1,18 @@
+from data.dataset import Dataset
+from Utils.kmeans import kMeans
+
+def preprocessing(dataset : Dataset, cluster = False):
+    dataset.dropDatasetColumns(["id"])
+    dataset.replaceBoolean("M", "B")
+    for column in dataset.getDataset().columns:
+        dataset.normalizeColumn(column)
+    
+    if cluster:
+        features = dataset.getDataFrame(['radius_mean', 'texture_mean', 'perimeter_mean'])
+        kmeans = kMeans().clustering(features)
+        dataset.addDatasetColumn('Appearance Cluster', kmeans.fit_predict(features))
+        dataset.dropDatasetColumns(columnsToRemove=['radius_mean', 'texture_mean', 'perimeter_mean'])
+        dataset.normalizeColumn('Appearance Cluster')
+        
+    dataset.dropDatasetColumns(['Unnamed: 32'])
+    return dataset
